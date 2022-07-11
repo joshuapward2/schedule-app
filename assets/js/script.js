aefa7ecaedd176efe1b6aa6fcba49ba9c702294f
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-11 13:02:10 -0400

    one version to remember not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,37 @@
    var todaysFullDate = moment().format('MMMM Do YYYY, h:mm:ss a');
    $("#currentDay").html(todaysFullDate);
 
-   console.log(todaysFullDate)
+//container, row, columns and buttons functionality//
+
+$(document).ready(function(){
+
+   $(".save-btn").on("click", function() {
+      var taskContent =$(this).siblings(".contentbox").val();
+
+      localStorage.saveT
+   
+   })
+   
+   const now = new Date();
+   const hours = now.getHours();
+   const timeTextEl =document.querySelector(".hour")
+
+   let bgColor;
+   if(hours === timeTextEl)
+      bgColor = "green";
+   else if(hours > timeTextEl)
+      bgColor = "red";
+   else if(hours < timeTextEl)
+      bgColor = "blue";
+
+      bgColor.innerHtml = now.toDateString();
+      timeTextEl.style.bgColor = bgColor;
+
+
+   
+
+
+})
 
 
 
