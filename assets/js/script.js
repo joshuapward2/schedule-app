cad2bc069289c0b69c28b3b321e2e394174f6a09
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-10 22:17:12 -0400

    rows and columns finally working

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Todays date and time will be displayed here//
+   var todaysFullDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+   $("#currentDay").html(todaysFullDate);
+
+   console.log(todaysFullDate)
+
+
+
+
+
+
+
+
+
+
+
+
+  
