b41702a2ca9d488687ce2d1a9c3deea88eb3b165
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-07-19 15:04:52 -0400

    almost there local working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,70 @@
 // Todays date and time will be displayed here//
+
    var todaysFullDate = moment().format('MMMM Do YYYY, h:mm:ss a');
    $("#currentDay").html(todaysFullDate);
 
 //container, row, columns and buttons functionality//
 
-$(document).ready(function(){
+ $(document).ready(function(){
 
-   $(".saveBtn").on("click", function() {
-      console.log($(this))
-      // var boxContent = $(this).siblings(".contentbox").val;
-      var timeSlot = $(this).parent().attr("id");
+    $(".saveBtn").on("click", function() {
+  
+    var boxContent = $(this).siblings(".contentbox").val();
+    var timeSlot = $(this).parent().attr("id");
+      // var boxContent = $(".contentbox").val();
+      //var timeSlot
     
       
    
 
-      localStorage.setItem(JSON.stringify(timeSlot, boxContent));
+       localStorage.setItem(timeSlot, boxContent);
    
-   })
-   var currentTime = moment().hour();
-   var currentBlock = $(".col-md-1 hour")
-   var textAreaEl = $(".col-md-10 contentbox")
+ })
+   var currentT = moment().hour();
+   var currentBlock = $(".time-block").attr('id');
+   var block8 = $("#8").attr('id');
+   var textAreaEl = $(".contentbox")
 
+   
+   // function timelooper(){
 
-   function timelooper(){
+   $("#8").children('.contentbox').val(localStorage.getItem("8"));
+   $("#9").children('.contentbox').val(localStorage.getItem("9"));
+   $("#10").children('.contentbox').val(localStorage.getItem("10"));
+   $("#11").children('.contentbox').val(localStorage.getItem("11"));
+   $("#12").children('.contentbox').val(localStorage.getItem("12"));
+   $("#13").children('.contentbox').val(localStorage.getItem("13"));
+   $("#14").children('.contentbox').val(localStorage.getItem("14"));
+   $("#15").children('.contentbox').val(localStorage.getItem("15"));
+   $("#16").children('.contentbox').val(localStorage.getItem("16"));
+   $("#17").children('.contentbox').val(localStorage.getItem("17"));
 
-   $("#hour0800 .contentbox").val(localStorage.getItem("hour0800"));
-   $("#hour0900 .contentbox").val(localStorage.getItem("hour0900"));
-   $("#hour1000 .contentbox").val(localStorage.getItem("hour1000"));
-   $("#hour1100 .contentbox").val(localStorage.getItem("hour1100"));
-   $("#hour1200 .contentbox").val(localStorage.getItem("hour1200"));
-   $("#hour1300 .contentbox").val(localStorage.getItem("hour1300"));
-   $("#hour1400 .contentbox").val(localStorage.getItem("hour1400"));
-   $("#hour1500 .contentbox").val(localStorage.getItem("hour1500"));
-   $("#hour1600 .contentbox").val(localStorage.getItem("hour1600"));
-   $("#hour1700 .contentbox").val(localStorage.getItem("hour1700"));
-
+   
 
          
-     $(".contentbox").each(function) 
+      //  $("#id").each(function(currentBlock,currentT){
+console.log(currentT)
+console.log(currentBlock)
 
+   if(currentT === currentBlock){
+      $(textAreaEl).addClass(".present")
 
-   if(currentTime === currentBlock){
-     $(textAreaEl).addClass(".present")
+    }
 
-   }
-
-   if(currentTime > currentBlock) {
-      $(textAreaEl).addClass(".future")
-
-
-   }
-
-   if(currentTime < currentBlock) {
+   if(currentT > currentBlock) {
       $(textAreaEl).addClass(".past")
+
+
    }
 
-     
-}
-timelooper();
-})
+   if(currentT < currentBlock) {
+     $(textAreaEl).addClass(".future")
+    }
+
+      //   function( moment.hour)
+// })
+
+ })
 
       
 
